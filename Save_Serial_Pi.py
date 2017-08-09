58e7c300934c2f34d8933ca0f57222b2f6f90249
Joe Sommer <jsommer1@stanford.edu>
2017-08-09 10:38:49 -0700

    Update Save_Serial_Pi.py

--- Save_Serial_Pi.py
+++ Save_Serial_Pi.py
@@ -7,10 +7,6 @@
 # data in 4 separate text files. 
 
 
-# This code is to be run from a Raspberry Pi 3. It'll hopefully read in serial data through the UART and save the raw 
-# data in 4 separate text files. 
-
-
 import serial 
 import time
 import struct
@@ -36,16 +32,16 @@ ser.open()
 # Creates 4 text files to save data from different leads: ECG1, ECG2, Resp, and PPG
 rightnow = datetime.datetime.now()
 
-ecg1 = open('ECG1DATA' + str(rightnow.isoformat()) + '.txt', 'a')  
-ecg2 = open('ECG2DATA' + str(rightnow.isoformat()) + '.txt', 'a')
-resp = open('RESPDATA' + str(rightnow.isoformat()) + '.txt', 'a')
-ppg = open('PPGDATA' + str(rightnow.isoformat()) + '.txt', 'a')
+ecg1 = open('ECG1DATA' + str(rightnow.isoformat()) + '.txt', 'ab')  
+ecg2 = open('ECG2DATA' + str(rightnow.isoformat()) + '.txt', 'ab')
+resp = open('RESPDATA' + str(rightnow.isoformat()) + '.txt', 'ab')
+ppg = open('PPGDATA' + str(rightnow.isoformat()) + '.txt', 'ab')
 
 # Compares checksum to sum of rest of data 
-checksum = open('CHECKSUM' + str(rightnow.isoformat()) + '.txt', 'a')
+#checksum = open('CHECKSUM' + str(rightnow.isoformat()) + '.txt', 'ab')
 
 #
-packnums = open('PACKNUMS' + str(rightnow.isoformat()) + '.txt', 'a')
+packnums = open('PACKNUMS' + str(rightnow.isoformat()) + '.txt', 'ab')
 
 # Syncs w/ beginning of a packet by clearing serial input and waiting for silent period between packets
 while True:                    
@@ -70,33 +66,42 @@ while True:
         for byte in packet: 
             byte = ser.read()
         #ecg1_entry = struct.unpack('<H', packet[2:4])
-        ecg1_entry = int.from_bytes(packet[2:4], byteorder='little', signed=False)
-        ecg1.write(str(ecg1_entry) + '\n')
+        #ecg1_entry = int.from_bytes(packet[2:4], byteorder='little', signed=False)
+        #ecg1.write(str(ecg1_entry) + '\n')
+        ecg1.write(packet[2] + ' ' + packet[3] + '\n')
+        
         #ecg2_entry = struct.unpack('<H', packet[4:6])
-        ecg2_entry = int.from_bytes(packet[4:6], byteorder='little', signed=False)
-        ecg2.write(str(ecg2_entry) + '\n')
+        #ecg2_entry = int.from_bytes(packet[4:6], byteorder='little', signed=False)
+        #ecg2.write(str(ecg2_entry) + '\n')
+        ecg2.write(packet[4] + ' ' + packet[5] + '\n')
+
+        
         #resp_entry = struct.unpack('<H', packet[6:8])
-        resp_entry = int.from_bytes(packet[6:8], byteorder='little', signed=False)
-        resp.write(str(resp_entry) + '\n')
+        #resp_entry = int.from_bytes(packet[6:8], byteorder='little', signed=False)
+        #resp.write(str(resp_entry) + '\n')
+        ecgresp.write(packet[6] + ' ' + packet[7] + '\n')
+        
         #ppg_entry = struct.unpack('<H', packet[8:10])
-        ppg_entry = int.from_bytes(packet[8:10], byteorder='little', signed=False)
-        ppg.write(str(ppg_entry) + '\n') 
+        #ppg_entry = int.from_bytes(packet[8:10], byteorder='little', signed=False)
+        #ppg.write(str(ppg_entry) + '\n') 
+        ppg.write(packet[8] + ' ' + packet[9] + '\n')
+        
         
         # Gets packet numbers 
-        packnum_entry = int.from_bytes(packet[0:2], byteorder='little', signed=False)
+        #packnum_entry = int.from_bytes(packet[0:2], byteorder='little', signed=False)
         # 
-        packnums.write(str(packnum_entry) + '\n')
-        
+        #packnums.write(str(packnum_entry) + '\n')
+        packnums.write(packet[0] + ' ' + packet[1] + '\n')
         
         # This part records the pack number & compares the checksum value to the sum of the data
-        checksum_entry = int.from_bytes(packet[10:12], byteorder='little', signed=False)
-        data_sum = ecg1_entry + ecg2_entry + resp_entry + ppg_entry 
+        #checksum_entry = int.from_bytes(packet[10:12], byteorder='little', signed=False)
+        #data_sum = ecg1_entry + ecg2_entry + resp_entry + ppg_entry 
         #
-        checksum.write('checksum: ' + str(checksum_entry) + ', data sum: ' + str(data_sum) + '\n')
+        #checksum.write('checksum: ' + str(checksum_entry) + ', data sum: ' + str(data_sum) + '\n')
         #checksum.write('pack number: ' + str(packnum_entry) + ' checksum: ' + str(checksum_entry) + ', data sum: ' + str(data_sum) + '\n')
-        if checksum_entry != data_sum:
-            checksum.write('DATA DOESN\'T ADD TO CHECKSUM, SOMETHING BAD HAPPENED UP HERE^^^ \n')
-            break 
+        #if checksum_entry != data_sum:
+        #    checksum.write('DATA DOESN\'T ADD TO CHECKSUM, SOMETHING BAD HAPPENED UP HERE^^^ \n')
+        #    break 
         
     current_time = time.process_time()
     if current_time - start_time >= TIMEOUT: 
@@ -108,7 +113,7 @@ ecg1.close()
 ecg2.close()  
 resp.close()  
 ppg.close()  
-checksum.close()
+#checksum.close()
 #
 packnums.close()
 if ser.is_open: 
