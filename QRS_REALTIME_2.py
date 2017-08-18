5d03dd3438b2497527f2b439bfcf2a06b01fe947
Joe Sommer <jsommer1@stanford.edu>
2017-08-18 16:52:25 -0700

    Update QRS_REALTIME_2.py

--- QRS_REALTIME_2.py
+++ QRS_REALTIME_2.py
@@ -335,7 +335,8 @@ while True:
     # be required. 
         data_length = len(ecg1)   # doesn't necessarily have to be ecg1, all waveforms have same length
         print(data_length)
-        if data_length >= ecg1_algorithm.winsizeEV - 1:
+        if data_length >= 192:  # technicality for filtering purposes 
+        #if data_length >= ecg1_algorithm.winsizeEV - 1:
             print(type(ecg1))
             ecg1_algorithm.iterate(ecg1, ECG1_QRS)
             ecg2_algorithm.iterate(ecg2, ECG2_QRS)
